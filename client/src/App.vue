9151ca01e6d777de3185bb5d0117d00634ecd003
Stanley008 <31549005+Stanley008@users.noreply.github.com>
2021-06-16 11:07:08 +0200

    Added Logo

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/tileCalculator.png">
 
     /Text sample change/
     <Calculations msg="Welcome to Your Vue.js App"/>
